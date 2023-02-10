7a2a3c4c536c4bd39b69791bf5a46aa8073566a7
HanifMDjamiludin <djamiludinh@gmail.com>
2023-02-10 18:01:56 -0500

    first Three ovals

--- changeImg.js
+++ changeImg.js
@@ -1,33 +1,14 @@
-// const backgroundImages = [
-//   "NextBest.jpg",
-//   "Dusk.jpg"
-// ];
-
-// let currentImageIndex = 0;
-
-// function updateBackgroundImage(){
-//   if (currentImageIndex == 0){
-
-//     document.querySelector(".u-section-1").style.backgroundImage = 'linear-gradient(0deg, rgba(51,51,41,0.45), rgba(51,51,41,0.45)), url("images/NextBest.jpg")';
-
-//     currentImageIndex = 1;
-//   }else{
-
-//     document.querySelector(".u-section-1").style.backgroundImage = 'linear-gradient(0deg, rgba(51,51,41,0.45), rgba(51,51,41,0.45)), url("images/Dusk.jpg")';
-
-//     currentImageIndex = 0;
-//   }
-
-// }
-// setInterval(updateBackgroundImage, 5000);
-
 var imageContainer = document.querySelector(".u-section-1");
 var imageArray = ['linear-gradient(0deg, rgba(51,51,41,0.45), rgba(51,51,41,0.45)), url("images/NextBest.jpg")', 'linear-gradient(0deg, rgba(51,51,41,0.45), rgba(51,51,41,0.45)), url("images/Dusk.jpg")'];
 var imageIndex = 0;
 
 function changeImage() {
   imageContainer.style.backgroundImage = imageArray[imageIndex];
-  imageIndex = (imageIndex + 1) % imageArray.length;
+  if(imageIndex == 0){
+    imageIndex=1;
+  }else{
+    imageIndex=0;
+  }
 }
 
 setInterval(changeImage, 5000);
