079a345c58bad487d4d21393cc041bf765b30464
HanifMDjamiludin <djamiludinh@gmail.com>
2023-02-18 20:16:54 -0500

    comments

--- changeImg.js
+++ changeImg.js
@@ -1,37 +1,4 @@
-// instant transition between two images
-// var imageContainer = document.querySelector(".u-section-1");
-// var imageArray = ["NextBest.jpg", "Dusk.jpg"];
-// var imageIndex = 0;
-
-// function changeImage() {
-//   imageContainer.style.backgroundImage = imageArray[imageIndex];
-//   if(imageIndex == 0){
-//     imageIndex=1;
-//   }else{
-//     imageIndex=0;
-//   }
-// }
-// setInterval(changeImage, 4300);
-
-
-
-
-
-//instant transition, linear gradient messes it up
-// const images = [
-//   "linear-gradient(rgba(51,51,41,0.45), rgba(51,51,41,0.45)), url('NextBest.jpg')",
-//   "linear-gradient(rgba(51,51,41,0.45), rgba(51,51,41,0.45)), url('Dusk.jpg')"
-// ];
-// let currentIndex = 0;
-
-// function changeBackground() {
-//   const section = document.querySelector(".u-section-1");
-//   currentIndex = (currentIndex + 1) % images.length;
-//   section.style.backgroundImage = `url(${images[currentIndex]})`;
-// }
-
-// setInterval(changeBackground, 5000);
-
+// navbar background color change on scroll
 var navbar = document.querySelector('.navbar');
 
 window.onscroll = function() {
@@ -42,7 +9,7 @@ window.onscroll = function() {
   }
 }
 
-
+// background image change
 const section = document.getElementById('sec-279e');
 const backgrounds = ['background1', 'background2'];
 let counter = 0;
